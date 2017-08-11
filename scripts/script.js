e9d3632e1a249a217cf46a55ff3e3f4079f2ed17
Lucas <viper20@poczta.fm>
2017-08-11 15:41:18 +0200

    carousel

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    console.log('Dom ready');
+
+    var carouselList = $('#js-myCarousel ul');
+    setInterval(changeSlide, 3000);
+
+
+  function changeSlide(){
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  }
+
+  function moveFirstSlide(){
+    var firstItem = $('#js-myCarousel').find('li:first');
+    var lastItem = $('#js-myCarousel').find('li:last');
+
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  }
+});
