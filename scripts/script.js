3e38a3bec29bf74a45b4687a781b545de074a5a1
Lucas <viper20@poczta.fm>
2017-08-15 13:40:40 +0200

    crysis

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,56 @@
-$(function(){
-
+$(function () {
+    'use strict';
     var carouselList = $('#js-myCarousel ul');
-    setInterval(changeSlide, 3000);
+    var interval = setInterval(changeSlide, 1000);
 
+//start karuzeli//
 
-  function changeSlide(){
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  }
+    function changeSlide() {
+        moveSlideLeft();
+        carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+    }
 
-  function moveFirstSlide(){
-    var firstItem = $('#js-myCarousel').find('li:first');
-    var lastItem = $('#js-myCarousel').find('li:last');
+    function moveFirstSlide() {
+        var firstItem = $('#js-myCarousel').find('li:first');
+        var lastItem = $('#js-myCarousel').find('li:last');
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+    }
 
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-  }
+//odwrócona kolejnośc przewijania//
+
+    function moveSlideLeft() {
+        var firstItem = $('#js-myCarousel').find('li:first');
+        var lastItem = $('#js-myCarousel').find('li:last');
+        lastItem.before(firstItem);
+        carouselList.css({marginLeft: 400});
+    }
+
+//strzałki przełaczające slajdy//
+
+    var arrowLeft = $('.arrow-left');
+    var arrowRight = $('.arrow-right');
+
+    arrowLeft.on('click', movePrevSlide);
+
+    function movePrevSlide() {
+        var firstItem = $('#js-myCarousel').find('li:first');
+        var lastItem = $('#js-myCarousel').find('li:last');
+        firstItem.before(lastItem);
+        carouselList.css({'marginLeft': -400});
+        carouselList.animate({'marginLeft': 0}, 500, clearInterval(interval));
+    }
+
+    arrowRight.on('click', function () {
+        carouselList.animate({marginLeft: -400}, 500, moveSlideNext);
+    });
+
+    function moveSlideNext() {
+        var firstItem = $('#js-myCarousel').find('li:first');
+        var lastItem = $('#js-myCarousel').find('li:last');
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+        clearInterval(interval);
+    }
+
 });
