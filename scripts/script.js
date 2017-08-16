247f6eacf379b9685fa575753c936c05a8f3510b
Lucas <viper20@poczta.fm>
2017-08-16 14:43:54 +0200

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
 $(function () {
-    'use strict';
+
     var carouselList = $('#js-myCarousel ul');
-    var interval = setInterval(changeSlide, 1000);
+    var interval = setInterval(changeSlideLeft, 1000);
 
 //start karuzeli//
 
-    function changeSlide() {
-        moveSlideLeft();
+    function changeSlideLeft() {
         carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
     }
 
@@ -19,38 +18,23 @@ $(function () {
 
 //odwrócona kolejnośc przewijania//
 
-    function moveSlideLeft() {
+    function moveLastSlide() {
         var firstItem = $('#js-myCarousel').find('li:first');
         var lastItem = $('#js-myCarousel').find('li:last');
-        lastItem.before(firstItem);
-        carouselList.css({marginLeft: 400});
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: -400});
     }
 
+    function changeSlideRight() {
+        moveLastSlide();
+        carouselList.animate({'marginLeft': 0}, 500);
+    }
 //strzałki przełaczające slajdy//
 
     var arrowLeft = $('.arrow-left');
     var arrowRight = $('.arrow-right');
 
-    arrowLeft.on('click', movePrevSlide);
-
-    function movePrevSlide() {
-        var firstItem = $('#js-myCarousel').find('li:first');
-        var lastItem = $('#js-myCarousel').find('li:last');
-        firstItem.before(lastItem);
-        carouselList.css({'marginLeft': -400});
-        carouselList.animate({'marginLeft': 0}, 500, clearInterval(interval));
-    }
-
-    arrowRight.on('click', function () {
-        carouselList.animate({marginLeft: -400}, 500, moveSlideNext);
-    });
-
-    function moveSlideNext() {
-        var firstItem = $('#js-myCarousel').find('li:first');
-        var lastItem = $('#js-myCarousel').find('li:last');
-        lastItem.after(firstItem);
-        carouselList.css({marginLeft: 0});
-        clearInterval(interval);
-    }
-
+    arrowLeft.on('click', changeSlideRight);
+    arrowRight.on('click', changeSlideLeft);
 });
+
