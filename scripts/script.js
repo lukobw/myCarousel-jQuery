6f63dc9cdf741c45e8c3da3afdfa455a8d294319
Lucas <viper20@poczta.fm>
2017-08-18 20:34:57 +0200

    new functions

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,19 @@ $(function () {
 //start karuzeli//
 
     function changeSlideLeft() {
-        carouselList.animate({'margin-left': -400}, 500, moveFirstSlide);
-        if (i === 3) {
+        if (i === 4) {
             clearInterval(interval);
             interval = setInterval(changeSlideRight, 1000);
             return;
         }
+        carouselList.animate({'margin-left': -400}, 500, moveFirstSlide);
         i++;
+        setActiveSlide(i);
     }
 
-    function moveFirstSlide() {
-        var firstItem = $('#js-myCarousel').find('li:first');
+    function moveFirstSlide(skipSlides) {
+        var firstItem = $('#js-myCarousel').find('li').slice(0, skipSlides);
+        // var firstItem = $('#js-myCarousel').find('li:first');
         var lastItem = $('#js-myCarousel').find('li:last');
         lastItem.after(firstItem);
         carouselList.css({marginLeft: 0});
@@ -33,14 +35,15 @@ $(function () {
     }
 
     function changeSlideRight() {
-        moveLastSlide();
-        carouselList.animate({'margin-left': 0}, 500);
         if (i === 0) {
             clearInterval(interval);
             interval = setInterval(changeSlideLeft, 1000);
             return;
         }
+        moveLastSlide();
+        carouselList.animate({'margin-left': 0}, 500);
         i--;
+        setActiveSlide(i);
     }
 
     function arrowClickRight() {
@@ -49,9 +52,16 @@ $(function () {
         clearInterval(interval);
     }
 
-    function arrowClickLeft() {
-        carouselList.animate({'margin-left': -400}, 500, moveFirstSlide);
-        clearInterval(interval);
+    // function arrowClickLeft() {
+    //     carouselList.animate({'margin-left': -400 * skipSlides}, 500, moveFirstSlide);
+    //     clearInterval(interval);
+    // }
+
+    function arrowClickLeft(skipSlides) {
+        carouselList.animate({'margin-left': -400 * skipSlides}, 500, function () {
+            moveFirstSlide(skipSlides);
+        });
+          clearInterval(interval);
     }
 
 //strzałki przełaczające slajdy//
@@ -60,7 +70,11 @@ $(function () {
     var arrowRight = $('.arrow-right');
 
     arrowLeft.on('click', arrowClickRight);
-    arrowRight.on('click', arrowClickLeft);
+    // arrowRight.on('click', arrowClickLeft);
+    arrowRight.on('click', function () {
+        skipSlides = 2;
+        arrowClickLeft(skipSlides);
+    });
 
     //Restart karuzeli po wciśnięciu strzałki//
 
@@ -70,6 +84,14 @@ $(function () {
 
     var start = $('.start');
     start.on('click', restart);
+
+    //kontrolki
+
+    function setActiveSlide(j) {
+        var $activeSlide = $('.indicators .item');
+        $activeSlide.removeClass('active');
+        $('.indicators .item').eq(j).addClass('active');
+    }
 
 });
 
